3047c789f4129b842c11a31e8b2b81e8faf8a495
previah <previah@gmail.com>
2026-08-14 10:29:12 +0200

    refactor: split redraw into functions
    the following functions are made:
    draw_scale_points
    draw_coordinate_system
    draw_barge_bollards
    draw_quay_bollards
    draw_mooring_lines

--- mooring_plan.py
+++ mooring_plan.py
@@ -509,8 +509,8 @@ class MooringPlanner:
         except:
             preserve_view = False
 
-        # Store current view if it exists
 
+        # Store current view if it exists
         if len(self.ax.images) > 0:
             self.current_xlim = self.ax.get_xlim()
             self.current_ylim = self.ax.get_ylim()
@@ -524,217 +524,20 @@ class MooringPlanner:
                 self.home_ylim = self.ax.get_ylim()
 
         # scale points
+        self.draw_scale_points()
 
-        for i, p in enumerate(self.scale_points):
-
-            self.ax.plot(
-                p[0],
-                p[1],
-                "ro"
-            )
-
-            self.ax.text(
-                p[0],
-                p[1],
-                f"S{i+1}",
-                color="red"
-            )
-
-        # origin
-
-        if self.project.origin is not None:
-
-            self.ax.plot(
-                self.project.origin[0],
-                self.project.origin[1],
-                marker="+",
-                markersize=15,
-                color="cyan"
-            )
-
-            self.ax.text(
-                self.project.origin[0],
-                self.project.origin[1],
-                "Origin",
-                color="cyan"
-            )
-
-        if (
-                self.project.origin is not None
-                and self.axis_point is not None
-        ):
-            ox, oy = self.project.origin
-            axp, ayp = self.axis_point
-
-            dx = axp - ox
-            dy = ayp - oy
-
-            axis_length = math.hypot(dx, dy)
-
-            ux = dx / axis_length
-            uy = dy / axis_length
-
-            vx = uy
-            vy = -ux
-
-            self.ax.plot(
-                [ox, ox + ux * axis_length],
-                [oy, oy + uy * axis_length],
-                color="cyan",
-                linewidth=2
-            )
-
-            self.ax.plot(
-                [ox, ox + vx * axis_length],
-                [oy, oy + vy * axis_length],
-                color="magenta",
-                linewidth=2
-            )
-
-            self.ax.text(
-                ox + ux * axis_length,
-                oy + uy * axis_length,
-                "X",
-                color="cyan"
-            )
-
-            self.ax.text(
-                ox + vx * axis_length,
-                oy + vy * axis_length,
-                "Y",
-                color="magenta"
-            )
-
-        if (
-                self.mode == "axis"
-                and self.project.origin is not None
-                and self.axis_preview is not None
-        ):
-            self.ax.plot(
-                [self.project.origin[0], self.axis_preview[0]],
-                [self.project.origin[1], self.axis_preview[1]],
-                "--",
-                color="cyan",
-                linewidth=2
-            )
-
-        if (
-                self.mode == "axis"
-                and self.project.origin is not None
-                and self.axis_preview is not None
-        ):
-            ox, oy = self.project.origin
-            px, py = self.axis_preview
-
-            # Rubber-band line
-            self.ax.plot(
-                [ox, px],
-                [oy, py],
-                "--",
-                color="cyan",
-                linewidth=2
-            )
-
-            # Angle relative to image X-axis
-            dx = px - ox
-            dy = oy - py
-
-            angle = math.degrees(
-                math.atan2(dy, dx)
-            )
-
-            # Angle label
-            self.ax.text(
-                px,
-                py,
-                f"{angle:.1f}°",
-                color="yellow",
-                fontsize=10,
-                bbox=dict(
-                    facecolor="black",
-                    alpha=0.7,
-                    edgecolor="none"
-                )
-            )
-
+        # origin/coordinate system
+        self.draw_coordinate_system()
 
         # barge
-
-        for name, p in self.barge_points.items():
-
-            if name == self.pending_line_start:
-                self.ax.plot(
-                    p[0],
-                    p[1],
-                    marker="s",
-                    markersize=14,
-                    color="yellow",
-                    markeredgecolor="black"
-                )
-            else:
-                self.ax.plot(
-                    p[0],
-                    p[1],
-                    "rs"
-                )
-
-            self.ax.text(
-                p[0],
-                p[1],
-                name,
-                color="red"
-            )
+        self.draw_barge_bollards()
 
         # quay
-
-        for name, p in self.quay_points.items():
-
-            if name == self.pending_line_end:
-                self.ax.plot(
-                    p[0],
-                    p[1],
-                    marker="o",
-                    markersize=14,
-                    color="yellow",
-                    markeredgecolor="black"
-                )
-            else:
-                self.ax.plot(
-                    p[0],
-                    p[1],
-                    "bo"
-                )
-
-            self.ax.text(
-                p[0],
-                p[1],
-                name,
-                color="blue"
-            )
+        self.draw_quay_bollards()
 
         # lines
+        self.draw_mooring_lines()
 
-        for line in self.lines:
-
-            p1 = self.barge_points[line["from"]]
-            p2 = self.quay_points[line["to"]]
-
-            self.ax.plot(
-                [p1[0], p2[0]],
-                [p1[1], p2[1]],
-                "g-",
-                linewidth=2
-            )
-
-            mx = (p1[0] + p2[0]) / 2
-            my = (p1[1] + p2[1]) / 2
-
-            self.ax.text(
-                mx,
-                my,
-                line["name"],
-                color="green"
-            )
 
         self.ax.set_title("Mooring Plan")
 
@@ -1566,6 +1369,217 @@ class MooringPlanner:
         )
 
         self.redraw()
+
+    def draw_scale_points(self):
+
+        for i, p in enumerate(self.scale_points):
+            self.ax.plot(
+                p[0],
+                p[1],
+                "ro"
+            )
+
+            self.ax.text(
+                p[0],
+                p[1],
+                f"S{i + 1}",
+                color="red"
+            )
+
+        # todo: the scale points cannot be changed once created.
+
+    def draw_coordinate_system(self):
+        if self.project.origin is not None:
+            self.ax.plot(
+                self.project.origin[0],
+                self.project.origin[1],
+                marker="+",
+                markersize=15,
+                color="cyan"
+            )
+
+            self.ax.text(
+                self.project.origin[0],
+                self.project.origin[1],
+                "Origin",
+                color="cyan"
+            )
+
+        if (
+                self.project.origin is not None
+                and self.axis_point is not None
+        ):
+            ox, oy = self.project.origin
+            axp, ayp = self.axis_point
+
+            dx = axp - ox
+            dy = ayp - oy
+
+            axis_length = math.hypot(dx, dy)
+
+            ux = dx / axis_length
+            uy = dy / axis_length
+
+            vx = uy
+            vy = -ux
+
+            self.ax.plot(
+                [ox, ox + ux * axis_length],
+                [oy, oy + uy * axis_length],
+                color="cyan",
+                linewidth=2
+            )
+
+            self.ax.plot(
+                [ox, ox + vx * axis_length],
+                [oy, oy + vy * axis_length],
+                color="magenta",
+                linewidth=2
+            )
+
+            self.ax.text(
+                ox + ux * axis_length,
+                oy + uy * axis_length,
+                "X",
+                color="cyan"
+            )
+
+            self.ax.text(
+                ox + vx * axis_length,
+                oy + vy * axis_length,
+                "Y",
+                color="magenta"
+            )
+
+        if (
+                self.mode == "axis"
+                and self.project.origin is not None
+                and self.axis_preview is not None
+        ):
+            self.ax.plot(
+                [self.project.origin[0], self.axis_preview[0]],
+                [self.project.origin[1], self.axis_preview[1]],
+                "--",
+                color="cyan",
+                linewidth=2
+            )
+
+        if (
+                self.mode == "axis"
+                and self.project.origin is not None
+                and self.axis_preview is not None
+        ):
+            ox, oy = self.project.origin
+            px, py = self.axis_preview
+
+            # Rubber-band line
+            self.ax.plot(
+                [ox, px],
+                [oy, py],
+                "--",
+                color="cyan",
+                linewidth=2
+            )
+
+            # Angle relative to image X-axis
+            dx = px - ox
+            dy = oy - py
+
+            angle = math.degrees(
+                math.atan2(dy, dx)
+            )
+
+            # Angle label
+            self.ax.text(
+                px,
+                py,
+                f"{angle:.1f}°",
+                color="yellow",
+                fontsize=10,
+                bbox=dict(
+                    facecolor="black",
+                    alpha=0.7,
+                    edgecolor="none"
+                )
+            )
+
+        # todo: undo does not apply to the origin. if bollards are created after the origin, the undo button will
+        # todo: remove the bollards and the origin, but redo will not work afterwards.
+
+
+    def draw_barge_bollards(self):
+        for name, p in self.barge_points.items():
+
+            if name == self.pending_line_start:
+                self.ax.plot(
+                    p[0],
+                    p[1],
+                    marker="s",
+                    markersize=14,
+                    color="yellow",
+                    markeredgecolor="black"
+                )
+            else:
+                self.ax.plot(
+                    p[0],
+                    p[1],
+                    "rs"
+                )
+
+            self.ax.text(
+                p[0],
+                p[1],
+                name,
+                color="red"
+            )
+
+    def draw_quay_bollards(self):
+        for name, p in self.quay_points.items():
+
+            if name == self.pending_line_end:
+                self.ax.plot(
+                    p[0],
+                    p[1],
+                    marker="o",
+                    markersize=14,
+                    color="yellow",
+                    markeredgecolor="black"
+                )
+            else:
+                self.ax.plot(
+                    p[0],
+                    p[1],
+                    "bo"
+                )
+
+            self.ax.text(
+                p[0],
+                p[1],
+                name,
+                color="blue"
+            )
+
+    def draw_mooring_lines(self):
+        for line in self.lines:
+            p1 = self.barge_points[line["from"]]
+            p2 = self.quay_points[line["to"]]
+
+            self.ax.plot(
+                [p1[0], p2[0]],
+                [p1[1], p2[1]],
+                "g-",
+                linewidth=2
+            )
+
+            mx = (p1[0] + p2[0]) / 2
+            my = (p1[1] + p2[1]) / 2
+
+            self.ax.text(
+                mx,
+                my,
+                line["name"],
+                color="green"
+            )
 
 root = tk.Tk()
 
